591ebc10edb16b74061b6666e0b5c2017b8615a8
milifrox <xinghao_2004@hotmail.com>
2014-09-06 18:04:41 -0400

    kill

--- HelloNymi/main.cpp
+++ HelloNymi/main.cpp
@@ -131,6 +131,7 @@ int main(int argc, const char* argv[]){
 				if (rssi>-30){
 					locked = 0;
 					std::cout << "log: unlocking\n";
+					system("taskkill /IM NymiSplash.exe");
 					///unlock
 				}
 			}
