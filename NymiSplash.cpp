53fc2986905186be6ffbf9af6686e2bfb7e6d6c1
Amer Hesson <hessonam@ecf.toronto.edu>
2014-09-06 13:55:17 -0400

    Added dat nymisplash.cpp

---
+++ NymiSplash.cpp
@@ -0,0 +1,12 @@
+// NymiSplash.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello World pls!!\n");
+	return 0;
+}
+
