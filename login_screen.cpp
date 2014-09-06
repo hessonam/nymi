909343ca719fc6ef1e1d96356ab0a2a7769e8511
jacorreia <jonathan.a.correia@gmail.com>
2014-09-06 13:27:21 -0400

    Added window for blocking the screen (not full screen yet)

---
+++ login_screen.cpp
@@ -0,0 +1,88 @@
+// include the basic windows header files and the Direct3D header file
+#include <windows.h>
+#include <windowsx.h>
+
+// define the screen resolution
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEX wc;
+
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = "WindowClass";
+
+    RegisterClassEx(&wc);
+
+    hWnd = CreateWindowEx(
+        NULL,
+        "WindowClass",
+        "Our Nymi Program",
+        WS_OVERLAPPEDWINDOW,
+        0, 0,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+
+    // set up and initialize Direct3D
+    initD3D(hWnd);
+
+    // enter the main loop:
+
+    MSG msg;
+
+    while(TRUE)
+    {
+        while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if(msg.message == WM_QUIT)
+            break;
+
+        render_frame();
+    }
+
+    // clean up DirectX and COM
+    cleanD3D();
+
+    return msg.wParam;
+}
+
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch(message)
+    {
+        case WM_DESTROY:
+            {
+                PostQuitMessage(0);
+                return 0;
+            } break;
+    }
+
+    return DefWindowProc (hWnd, message, wParam, lParam);
+}
